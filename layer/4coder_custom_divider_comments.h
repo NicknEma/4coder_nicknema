eda429f3a05cbdc202969cc6758f5b13636f7b5e
NicknEma <62065135+NicknEma@users.noreply.github.com>
2024-10-28 15:00:33 +0100

    Cleanup 4coder_custom_divider_comments.*
    Make wrappers for both 'strong' and 'weak' dividers,
    Fix the signature,
    Update command documentation,

--- layer/4coder_custom_divider_comments.h
+++ layer/4coder_custom_divider_comments.h
@@ -1,10 +1,10 @@
 #ifndef FCODER_CUSTOM_DIVIDER_COMMENTS_H
 #define FCODER_CUSTOM_DIVIDER_COMMENTS_H
 
-namespace nne {
-	
-	function void render_divider_comments(Application_Links *app, Buffer_ID buffer, View_ID view, Text_Layout_ID text_layout_id);
-	
-}
+NAMESPACE_BEGIN(nne)
+
+function void render_divider_comments(Application_Links *app, Buffer_ID buffer, View_ID view, Text_Layout_ID text_layout_id);
+
+NAMESPACE_END()
 
 #endif // FCODER_CUSTOM_DIVIDER_COMMENTS_H
